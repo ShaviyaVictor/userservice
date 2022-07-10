4d3f981faceb8cb6afffff68fa094a4fbcc00fa3
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-10 18:37:22 -0500

    Create the constructor initializer for our User object

--- src/main/java/io/getarrays/userservice/domain/User.java
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -1,7 +1,15 @@
 package io.getarrays.userservice.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class User {
 
-
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<Role> roles = new ArrayList<>();
 
 }
