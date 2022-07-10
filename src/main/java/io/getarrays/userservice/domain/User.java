179ff7332430a59a426b0a3115e84a4ef1ba3805
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-10 18:53:37 -0500

    Add annotations on User Java Class to map the data as tables

--- src/main/java/io/getarrays/userservice/domain/User.java
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -1,5 +1,10 @@
 package io.getarrays.userservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import java.util.ArrayList;
@@ -7,6 +12,7 @@ import java.util.Collection;
 
 import static javax.persistence.FetchType.EAGER;
 
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
 
     private Long id;
