b53f3c3e4475e368bfc01c4bf3d80afa5211945c
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-10 18:56:48 -0500

    Add the ID annotations that identifies the id as well as declare how I want the id to be generated using the @GeneratedValue annotation

--- src/main/java/io/getarrays/userservice/domain/User.java
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.GenerationType.AUTO;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
 
+    @Id @GeneratedValue(strategy = AUTO)
     private Long id;
     private String name;
     private String username;
