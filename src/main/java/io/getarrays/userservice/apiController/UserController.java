f7183cac196bf6063b6fc13441d89a479dc5777e
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 09:51:44 -0500

    Create a method to return a ResponseEntity of all the users in the DB

--- src/main/java/io/getarrays/userservice/apiController/UserController.java
+++ src/main/java/io/getarrays/userservice/apiController/UserController.java
@@ -1,10 +1,23 @@
 package io.getarrays.userservice.apiController;
 
+import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api")
 public class UserController {
 
     private final UserService userService;
+
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 
 }
