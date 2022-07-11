dd1168955f92483fd6c2e74d5350840bb84ac570
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 15:24:10 -0500

    Disable CSRF of the http

--- src/main/java/io/getarrays/userservice/security/SecurityConfig.java
+++ src/main/java/io/getarrays/userservice/security/SecurityConfig.java
@@ -24,6 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
     }
 }
