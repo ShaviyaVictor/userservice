d856e8ab66173bc472e9fc6605eb9fcd4b1cc889
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 08:18:42 -0500

    Create the logs for the methods created in the ServiceImplementation

--- src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
@@ -21,16 +21,19 @@ public class UserServiceImplementation implements UserService {
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database.", user.getName());
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database.", role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to a user.", roleName, username);
         User user = userRepository.findByUsername(username);
         Role role = roleRepository.findByName(roleName);
         user.getRoles().add(role);
@@ -38,11 +41,13 @@ public class UserServiceImplementation implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("Getting the user {}.", username);
         return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users.");
         return userRepository.findAll();
     }
 }
