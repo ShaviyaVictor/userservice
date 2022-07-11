fe4178b761bae5dba0ab6ca6691942a633b62530
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 09:33:50 -0500

    Create the UserController and inject the UserService for the controller

---
+++ src/main/java/io/getarrays/userservice/apiController/UserController.java
@@ -0,0 +1,10 @@
+package io.getarrays.userservice.apiController;
+
+import io.getarrays.userservice.service.UserService;
+
+
+public class UserController {
+
+    private final UserService userService;
+
+}
