0fa2d5441ee1c366c489109fd590e7e2608ec6e7
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-10 20:27:03 -0500

    Create service folder and create methods for the UserService

---
+++ src/main/java/io/getarrays/userservice/service/UserService.java
@@ -0,0 +1,30 @@
+package io.getarrays.userservice.service;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+
+import java.util.List;
+
+public class UserService {
+
+    User saveUser(User user) {
+        return null;
+    }
+
+    Role saveRole(Role role) {
+        return null;
+    }
+
+    void addRoleToUser(String username, String roleName) {
+
+    }
+
+    User getUser(String username) {
+        return null;
+    }
+
+    List<User> getUsers() {
+        return null;
+    }
+
+}
