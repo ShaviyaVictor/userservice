f706d61ef62d032695aa40e9451b9266d21b48c8
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 13:15:08 -0500

    Create instances of the objects for the tables for testing purposes

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -1,7 +1,13 @@
 package io.getarrays.userservice;
 
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -12,6 +18,44 @@ public class UserserviceApplication {
 
 		/*System.out.println("Yes! A weekend well spent. Thank You Jesus!");*/
 
+	}
+
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null,
+					"Victor Shaviya",
+					"shavic",
+					"shavic@gmail.com",
+					"123456",
+					new ArrayList<>()
+					));
+			userService.saveUser(new User(null,
+					"Josphine Mbaisi",
+					"jomba",
+					"jomba@gmail.com",
+					"123456",
+					new ArrayList<>()
+			));
+			userService.saveUser(new User(null,
+					"Norris Ambune",
+					"norra",
+					"norra@gmail.com",
+					"123456",
+					new ArrayList<>()
+			));
+			userService.saveUser(new User(null,
+					"Lloyd McBean",
+					"mc",
+					"mc@gmail.com",
+					"123456",
+					new ArrayList<>()
+			));
+		};
 	}
 
 }
