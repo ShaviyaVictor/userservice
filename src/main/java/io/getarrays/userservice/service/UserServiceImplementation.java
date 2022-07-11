daaeb1f0396b31641bd394bf62cf2aebd6294a43
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 14:49:44 -0500

    Looping over the uathorities available from Spring Security and creating a grand authority

--- src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
@@ -6,12 +6,15 @@ import io.getarrays.userservice.repository.RoleRepository;
 import io.getarrays.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
@@ -25,7 +28,18 @@ public class UserServiceImplementation implements UserService, UserDetailsServic
     // userDetailsImplementation
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username);
+        if(user == null) {
+            log.error("User not found!");
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            log.info("User with the username {} found!", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 
     @Override
