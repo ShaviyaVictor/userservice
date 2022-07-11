7516994c12a856282c2102e4b855f3169a4896c6
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 14:30:46 -0500

    Introduce a Bean to the main clss for generating a new BCryptPasswordEncoder

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -14,11 +16,13 @@ import java.util.ArrayList;
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(UserserviceApplication.class, args);
-
 		/*System.out.println("Yes! A weekend well spent. Thank You Jesus!");*/
+	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
