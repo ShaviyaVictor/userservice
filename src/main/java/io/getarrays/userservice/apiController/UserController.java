7f7308d21069968b808d0611a718da7013692bec
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-11 09:58:14 -0500

    Add the api endpoint for the list of users returned by the first method implemented by the GetMapping annotation

--- src/main/java/io/getarrays/userservice/apiController/UserController.java
+++ src/main/java/io/getarrays/userservice/apiController/UserController.java
@@ -4,6 +4,7 @@ import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping(path = "/users")
     public ResponseEntity<List<User>>getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
