0fe267ce890f1ccd21c45c645a064f59a274e105
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-12 09:53:11 -0500

    Create a refresh token which expires after 30 days

--- src/main/java/io/getarrays/userservice/filter/CustomAuthFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthFilter.java
@@ -46,9 +46,17 @@ public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         User user = (User) authentication.getPrincipal();
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        // the Expires is after 10 minutes
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining()))
+                .sign(algorithm);
+        // the ExpiresAt is after 30 days or something longer (24hrs, 60min, 60sec, 1000millisec)
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 24 * 60 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining()))
                 .sign(algorithm);
