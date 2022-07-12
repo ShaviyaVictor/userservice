a5b9e1b593fe911d187c2e0d1fca87e281545edb
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-12 13:46:10 -0500

    Single out the credentials of a user and set the user up in the Security context of Spring

--- src/main/java/io/getarrays/userservice/filter/CustomAuthorizationFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthorizationFilter.java
@@ -4,7 +4,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -37,6 +39,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                     stream(roles).forEach(role -> {
                         authorities.add(new SimpleGrantedAuthority(role));
                     });
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                 }catch (Exception exception) {
 
                 }
