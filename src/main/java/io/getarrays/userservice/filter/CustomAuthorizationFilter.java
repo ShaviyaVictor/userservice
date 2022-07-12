319ee02ecbe63ecf0af39ae0e35ab95210177552
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-12 13:27:00 -0500

    Decode the verified token

--- src/main/java/io/getarrays/userservice/filter/CustomAuthorizationFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthorizationFilter.java
@@ -3,6 +3,7 @@ package io.getarrays.userservice.filter;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -24,6 +25,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                 String token = authorizationHeader.substring("Bearer ".length());
                 Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                 JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(token);
             }
         }
     }
