7a251a0cefea3791c71d7265417fa3087d5bdb27
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-12 12:11:27 -0500

    Declare a response and set the type to JSON type of content mapped to the body

--- src/main/java/io/getarrays/userservice/filter/CustomAuthFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthFilter.java
@@ -2,6 +2,7 @@ package io.getarrays.userservice.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,6 +21,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
@@ -65,5 +68,7 @@ public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
         tokens.put("refresh_token", refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
