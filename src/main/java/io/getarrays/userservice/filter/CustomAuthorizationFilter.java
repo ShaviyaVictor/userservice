53c24ea88e048d2d37bf65f121ce5d5d32e1a2e2
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-12 13:04:16 -0500

    Create the CustomAuthorizationFilter for authorization while implementing OncePerRequestFilter

---
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,16 @@
+package io.getarrays.userservice.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+    }
+}
